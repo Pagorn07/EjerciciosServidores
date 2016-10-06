484e4ce9feac4a56e7ee753a3d5ec64d4af1fffd
arbos.pablo <pabloarbos1993@gmail.com>
2016-10-06 20:46:12 +0200

    LA primerisima practica de todas V0.2

--- src/PrimeraPractica/GraficWindow.java
+++ src/PrimeraPractica/GraficWindow.java
@@ -6,10 +6,40 @@ import java.awt.*;
 
 public class GraficWindow extends JFrame{
     private JPanel contentPane;
-    private String type = "";
+    private String optionType = "";
+    private String text = "";
+    private boolean fillColor = false;
+    private int squareSize = 0;
 
-    public GraficWindow(String type) {
-        this.type = type;
+    public String getOptionType() {
+        return optionType;
+    }
+
+    public void setOptionType(String optionType) {
+        this.optionType = optionType;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setSquareSize(int squareSize) {
+        this.squareSize = squareSize;
+    }
+
+    public int getSquareSize() {
+        return squareSize;
+    }
+
+    public void setFillColor(boolean fillColor) {
+        this.fillColor = fillColor;
+    }
+
+    public GraficWindow() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
         contentPane = new JPanel();
@@ -23,10 +53,18 @@ public class GraficWindow extends JFrame{
     public void paint(Graphics g) {
         super.paint(g);
 
-        if(type.equals("text")) {
-            g.drawString("hola", 10, 200);
-        }
+        g.setColor(Color.blue);
 
+        if(optionType.equals("string")) {
+            g.drawString(text, 10, 200);
+        } else if(optionType.equals("square")) {
+            if (fillColor) {
+                g.fillRect(100, 100, squareSize, squareSize);
+            } else {
+                g.setColor(Color.magenta);
+                g.drawRect(100, 100, squareSize, squareSize);
+            }
+        }
     }
 
 }
--- src/PrimeraPractica/Main.java
+++ src/PrimeraPractica/Main.java
@@ -4,8 +4,13 @@ import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
+    private static String optionType = "";
+    private static String text = "";
+    private static boolean fillColor = false;
+    private static int squareSize = 0;
+    private static Scanner sc = new Scanner(System.in);
+
     static void menu() {
-        Scanner sc = new Scanner(System.in);
         String answer = "";
 
         System.out.print("Quina figura vols dibuixar (escull una lletra)?\n\n" +
@@ -24,15 +29,17 @@ public class Main {
 
         switch (answer) {
             case "A":
-                showText("text");
+                showText();
                 break;
             case "B":
+                showPoint();
                 break;
             case "C":
                 break;
             case "D":
                 break;
             case "E":
+                showSquare();
                 break;
             case "F":
                 break;
@@ -48,11 +55,15 @@ public class Main {
         }
     }
 
-    static void callWindow(final String text) {
+    static void callWindow() {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    GraficWindow frame = new GraficWindow(text);
+                    GraficWindow frame = new GraficWindow();
+                    frame.setOptionType(optionType);
+                    frame.setText(text);
+                    frame.setSquareSize(squareSize);
+                    frame.setFillColor(fillColor);
                     frame.setVisible(true);
                 } catch(Exception e) {
                     e.printStackTrace();
@@ -61,8 +72,25 @@ public class Main {
         });
     }
 
-    static void showText(String type) {
-        callWindow(type);
+    static void showText() {
+        optionType = "string";
+        System.out.print("Escribe un texto --> ");
+        text = sc.nextLine();
+        callWindow();
+    }
+
+    static void showPoint() {
+        callWindow();
+    }
+
+    static void showSquare() {
+        optionType = "square";
+        System.out.print("De que dimension quieres que sea el cuadrado? --> ");
+        squareSize = sc.nextInt();
+        System.out.print("Lo quieres relleno de color?(S | N) --> ");
+        if(sc.next().equals("S")) fillColor = true;
+        else fillColor = false;
+        callWindow();
     }
 
     public static void main(String[] args) {
